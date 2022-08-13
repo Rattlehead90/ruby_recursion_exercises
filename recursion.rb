f60e781f8ba80a9ad9ed4df27c2ccda2210ffce7
Rattlehead90 <strogonov@seznam.cz>
2022-08-13 12:52:33 +0200

    Fibonacci sequence using iteration

--- recursion.rb
+++ recursion.rb
@@ -1,1 +1,20 @@
+# fibonacci sequence
 
+# using iteration
+def fibs(n)
+  sequence = [0, 1]
+  if n == 0 || n == 1 
+    return [0] 
+  elsif n == 2
+    return [0, 1]
+  else
+    i = 2
+    while sequence.length < n
+      sequence[i] = (sequence[i - 1] + sequence[i - 2])
+      i += 1 
+    end
+  end
+  sequence.to_s
+end
+
+puts fibs(8)
