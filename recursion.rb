8cfa7d45f19a326512a7e07fa472a7975eb7d4a0
Rattlehead90 <strogonov@seznam.cz>
2022-08-13 13:09:48 +0200

    Fibonacci sequence using recursion

--- recursion.rb
+++ recursion.rb
@@ -17,4 +17,14 @@ def fibs(n)
   sequence.to_s
 end
 
-puts fibs(8)
+puts "Fibonacci using iteration #{fibs(8)}"
+
+# using recursion
+def fibs_rec(n)
+  return [0] if n == 1 || n == 0
+  return [0,1] if n == 2 
+  sequence = fibs_rec(n - 1) 
+  sequence << sequence[-2] + sequence[-1]
+end
+
+puts "Fibonacci using recursion #{fibs_rec(8)}"
