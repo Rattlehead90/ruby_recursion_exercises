13da402031b26897787ca2893f804ab990c54077
Rattlehead90 <strogonov@seznam.cz>
2022-08-13 13:59:04 +0200

    Merge sort

--- recursion.rb
+++ recursion.rb
@@ -28,3 +28,14 @@ def fibs_rec(n)
 end
 
 puts "Fibonacci using recursion #{fibs_rec(8)}"
+
+# merge sort algorithm
+def merge_sort(arr)
+  return arr unless arr.size > 1
+  mid = arr.size/2
+  a, b, sorted = merge_sort(arr[0...mid]), merge_sort(arr[mid..-1]), []
+  sorted << (a[0] < b[0] ? a.shift : b.shift) while [a,b].none?(&:empty?)
+  sorted + a + b
+end
+
+puts "Sorted array: #{merge_sort([8, 4, 6, 12, 5])}"
